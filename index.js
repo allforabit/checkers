852b94bce5ef268e18fc3ecc4f49f464d6509685
Kevin Nolan <kevnolan@gmail.com>
2015-03-13 12:18:39 +0000

    fix e

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var compression = require('compression');
 var app = express();
 
-var oneDay = 86400000;
+app.use(express.static(__dirname + '/'));
 
-app.use(compression);
-
-app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
-
-app.use(bodyParser);
-
-app.listen(process.env.PORT);
-
-app.post('/', function(req, res){
-  var result = req.rawBody;
-  res.send("hello there world data is " + result);
-});
+app.listen(9999);
